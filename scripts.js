59dad07b667ccf26ceacbb4ca562731511133981
thenoodlewarrior <nmmenrath@gmail.com>
2022-03-08 12:28:39 -0500

    Got a single round to work

--- scripts.js
+++ scripts.js
@@ -5,25 +5,30 @@ let choices = [
 ];
 
 
-function computerPlay() {
-    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-};
+
 
 
 
 
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection == "rock") {
+    playerSelection = "rock";
+    computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    console.log(computerSelection);
+
+    if (computerSelection == playerSelection) {
         console.log("Tie Game!")
     }
 
     else {
         console.log("Undecided!")
     }
+    
    
+    
 };
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection));
+
+
+
+playRound()
