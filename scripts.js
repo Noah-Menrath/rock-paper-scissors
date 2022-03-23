9fa76af04ae01c759ef87b3e319d617180326b6b
thenoodlewarrior <nmmenrath@gmail.com>
2022-03-23 00:13:08 -0400

    Added button functions and working on visual scoreboard along with added some css styling

--- scripts.js
+++ scripts.js
@@ -11,93 +11,96 @@ const rock = document.querySelector('#btn1');
 const paper = document.querySelector('#btn2');
 const scissors = document.querySelector('#btn3');
 
+let linkPlayerSelection = document.querySelector('#htmlPlayerSelection');
+let linkComputerSelection = document.querySelector('#htmlComputerSelection');
 
 
 
-function playRound() {
-
-   
+/* if player chose rock */
+function rockChoice() {
 
     /* computer randomly chooses either rock, paper, or scissors */
     computerSelection = choices[Math.floor(Math.random() * choices.length)];
-    console.log(`The computer picked ${computerSelection}. You picked ${playerSelection} .`);
+    console.log(`You picked Rock.`);
+    console.log(`The computer picked ${computerSelection}.`)
 
-    /* Method 1: */
-    if (playerSelection == "rock" && computerSelection == "scissors") {
+    
+    if (computerSelection == "scissors") {
         console.log("Rock wins! Congratulations!");
+        linkPlayerSelection.textContent = "You Chose Rock!";
+        linkComputerSelection.textContent="The Computer Chose Scissors!"
     }
-
-
-    
-    
-    /* Method 2: */
-    else if (document.getElementById('#btn1').clicked == true && computerSelection == "paper") {
+    else if (computerSelection == "paper") {
         console.log("Paper wins! You Lose!");
     }
 
-    else if (document.getElementById('#btn1').clicked == true && computerSelection == "rock") {
+    else if (computerSelection == "rock") {
         console.log("Both Chose Rock! Tie Game!");
     }
 
-    else if (document.getElementById('#btn2').clicked == true && computerSelection == "scissors") {
+    else {
+        console.log("Unexpected Error");
+    }
+};
+
+/* if player chose paper */
+function paperChoice() {
+
+     /* computer randomly chooses either rock, paper, or scissors */
+     computerSelection = choices[Math.floor(Math.random() * choices.length)];
+     console.log(`You picked Paper!`);
+     console.log(`The computer picked ${computerSelection}.`);
+
+
+    if (computerSelection == "scissors") {
         console.log("Scissors Beat Paper! You Lose!");
     }
 
-    else if (document.getElementById('#btn2').clicked == true && computerSelection == "rock") {
+    else if (computerSelection == "rock") {
         console.log("Paper Beats Rock! Congratulations!");
     }
 
-    else if (document.getElementById('#btn2').clicked == true && computerSelection == "paper") {
+    else if (computerSelection == "paper") {
         console.log("Both Chose Paper! Tie Game!");
-    }
-
-    else if (document.getElementById('#btn3').clicked == true && computerSelection == "rock") {
-        console.log("Rock Beats Scissors! You Lose!");
-    }
-
-    else if (document.getElementById('#btn3').clicked == true && computerSelection == "paper") {
-        console.log("Scissors Beat Paper! Congratulations!");
-    }
-
-    else if (document.getElementById('#btn3').clicked == true && computerSelection == "scissors") {
-        console.log("Both Chose Paper! Tie Game");
     }
 
     else {
         console.log("Unexpected Error");
     }
 
-    console.log(playerSelection);
-    
-
-    
-
-
-   
-    
 };
-function funGame() {
-    if (document.getElementById('#btn1').clicked == true) {
-        playerSelection = "rock";
-        playRound();
-    } else {
-        playerSelection = "unavailable";
+
+/* if player chose scissors */
+function scissorsChoice() {
+
+     /* computer randomly chooses either rock, paper, or scissors */
+     computerSelection = choices[Math.floor(Math.random() * choices.length)];
+     console.log(`You picked Scissors!`);
+     console.log(`The computer picked ${computerSelection}.`);
+
+
+    if (computerSelection == "rock") {
+        console.log("Rock Beats Scissors! You Lose!");
     }
+
+    else if (computerSelection == "paper") {
+        console.log("Scissors Beat Paper! Congratulations!");
+    }
+
+    else if (computerSelection == "scissors") {
+        console.log("Both Chose Paper! Tie Game!");
+    }
+
+    else {
+        console.log("Unexpected Error");
+    }
+
+
 };
 
 
 
-rock.addEventListener("click", playRound);
-paper.addEventListener("click", playRound);
-scissors.addEventListener("click", playRound);
 
-function game() {
-    playRound()
-    };
-
-
-
-
-
-
-/*game() */ 
+rock.addEventListener("click", rockChoice);
+paper.addEventListener("click", paperChoice);
+scissors.addEventListener("click", scissorsChoice);
